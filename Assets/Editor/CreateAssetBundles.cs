e2adc8bef08b8c17c632db8a01e419d1ba2f8c73
Hoang Vo <hoangvod@gmail.com>
2021-04-10 10:15:21 -0500

    Add Bundle Object

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class CreateAssetBundles : MonoBehaviour
+{
+   [MenuItem("Assets/Build AssetBundles")]
+   static void BuildAllAssetBundles()
+    {
+        if (!Directory.Exists(Application.streamingAssetsPath)){
+        	Directory.CreateDirectory("Assets/StreamingAssets");
+        }
+
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets", BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+
+    }
+}
---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class LoadAssets : MonoBehaviour
+{
+    public string assetName = "chair1";
+    public string bundleName = "chair";
+    public GameObject asset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
+        if (localAssetBundle == null)
+        {
+            Debug.LogError("Failed to load AssetBundle!");
+            return;
+        }
+        asset = localAssetBundle.LoadAsset<GameObject>(assetName);
+        Instantiate(asset);
+        localAssetBundle.Unload(false);
+        //string url = "http://filmbuzzer.com/AssetBundles/Chair";
+     //   WWW www = new WWW(url);
+      //  StartCoroutine(WaitForReq(www));
+
+    }
+
+    IEnumerator WaitForReq(WWW www)
+    {
+        yield return www;
+        AssetBundle bunlde = www.assetBundle;
+        if (www.error == null)
+        {
+            //GameObject asset = localAssetBundle.LoadAsset<GameObject>("chair1");
+            //    (GameObject)AssetBundle.LoadAsset("Chair");
+           // Instantiate(asset);
+        }
+        else
+        {
+            Debug.Log(www.error);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
