0b8e1416fc22955c6b42d4f5791f75d1385b04ca
Hoang Vo <hoangvod@gmail.com>
2021-04-11 10:41:44 -0500

    done with interacting with bundle asset, build the basic functions later, adding AWS 3s

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -9,9 +9,11 @@ public class LoadAssets : MonoBehaviour
     public string assetName = "chair1";
     public string bundleName = "chair";
     public GameObject obj;
+    public GameObject parentObj;
     // Start is called before the first frame update
     void Start()
     {
+        obj.transform.position = parentObj.transform.position;
         AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, bundleName));
         if (localAssetBundle == null)
         {
@@ -19,34 +21,103 @@ public class LoadAssets : MonoBehaviour
             return;
         }
         GameObject asset = localAssetBundle.LoadAsset<GameObject>(assetName);
+      // obj = Instantiate(asset) as GameObject;
+        // obj.transform.localScale += new Vector3(10F, 10F, 10F);
+      //asset.transform.SetParent(obj.transform.parent, false);
         obj = Instantiate(asset) as GameObject;
-        obj.transform.localScale += new Vector3(10F, 0, 0);
+        obj.transform.SetParent(parentObj.transform, false);
         localAssetBundle.Unload(false);
+        ////////////////
+        ///
+
+
         //string url = "http://filmbuzzer.com/AssetBundles/Chair";
-     //   WWW www = new WWW(url);
-      //  StartCoroutine(WaitForReq(www));
+        //   WWW www = new WWW(url);
+        //  StartCoroutine(WaitForReq(www));
 
     }
 
-    IEnumerator WaitForReq(WWW www)
-    {
-        yield return www;
-        AssetBundle bunlde = www.assetBundle;
-        if (www.error == null)
-        {
-            //GameObject asset = localAssetBundle.LoadAsset<GameObject>("chair1");
-            //    (GameObject)AssetBundle.LoadAsset("Chair");
-           // Instantiate(asset);
-        }
-        else
-        {
-            Debug.Log(www.error);
-        }
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+       if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+                AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "furniture"));
+                GameObject asset = localAssetBundle.LoadAsset<GameObject>("furniture1");
+                obj = Instantiate(asset) as GameObject;
+                obj.transform.SetParent(parentObj.transform, false);
+                localAssetBundle.Unload(false);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+                AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "bedsidetable"));
+                GameObject asset = localAssetBundle.LoadAsset<GameObject>("bedsidetable");
+                obj = Instantiate(asset) as GameObject;
+                obj.transform.SetParent(parentObj.transform, false);
+                localAssetBundle.Unload(false);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+                AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "chair"));
+                GameObject asset = localAssetBundle.LoadAsset<GameObject>("chair1");
+                obj = Instantiate(asset) as GameObject;
+                obj.transform.SetParent(parentObj.transform, false);
+                localAssetBundle.Unload(false);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+                AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "plant"));
+                GameObject asset = localAssetBundle.LoadAsset<GameObject>("plant1");
+                obj = Instantiate(asset) as GameObject;
+                obj.transform.SetParent(parentObj.transform, false);
+                localAssetBundle.Unload(false);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+                AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "sofa"));
+                GameObject asset = localAssetBundle.LoadAsset<GameObject>("sofa");
+                obj = Instantiate(asset) as GameObject;
+                obj.transform.SetParent(parentObj.transform, false);
+                localAssetBundle.Unload(false);
+            }
+        }
+
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+                AssetBundle localAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "tablewood"));
+                GameObject asset = localAssetBundle.LoadAsset<GameObject>("bedsidetablewood");
+                obj = Instantiate(asset) as GameObject;
+                obj.transform.SetParent(parentObj.transform, false);
+                localAssetBundle.Unload(false);
+            }
+        }
     }
 }
