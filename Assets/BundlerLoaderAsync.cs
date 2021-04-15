221281a69d5d67824a29ad200cecfb1e83805265
Hoang Vo <hoangvod@gmail.com>
2021-04-14 21:37:03 -0500

    No AWS

--- Assets/BundlerLoaderAsync.cs
+++ Assets/BundlerLoaderAsync.cs
@@ -11,7 +11,8 @@ public class BundlerLoaderAsync : MonoBehaviour
     void Start()
     {
         obj.transform.position = parentObj.transform.position;
-        string url = "https://bundle-asset.s3.amazonaws.com/BundledAssets/sofa.fbx";
+        string url = "https://bundle-asset.s3.amazonaws.com/StreamingAssets/sofa";
+        //string url = "https://drive.google.com/uc?export=download&id=1CrzXJj76H0uGGMUl-r7cOjynORem1QF9";
         WWW www = new WWW(url);
         StartCoroutine(WaitForReq(www));
     }
@@ -23,13 +24,15 @@ public class BundlerLoaderAsync : MonoBehaviour
         AssetBundle bundle = www.assetBundle;
         if (www.error == "")
         {
-            GameObject sofa = bundle.LoadAsset<GameObject>("sofa");
-            obj = Instantiate(sofa) as GameObject;
-            obj.transform.SetParent(parentObj.transform, false);
+            GameObject sofa = (GameObject)bundle.LoadAsset("sofa");
+            Instantiate(sofa);
+            //obj = Instantiate(sofa); //as GameObject;
+            //obj.transform.SetParent(parentObj.transform, false);
         } else
         {
             Debug.Log(www.error);
         }
+        bundle.Unload(false);
     }
     // Update is called once per frame
     void Update()
